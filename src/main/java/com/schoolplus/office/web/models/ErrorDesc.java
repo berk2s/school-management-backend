713bfc67e45e032588fd90b5207425266d41df3b
berk2s <berktopcu@outlook.com>
2021-08-31 03:12:02 +0300

    error descriptions are refactored

--- src/main/java/com/schoolplus/office/web/models/ErrorDesc.java
+++ src/main/java/com/schoolplus/office/web/models/ErrorDesc.java
@@ -2,32 +2,46 @@ package com.schoolplus.office.web.models;
 
 import lombok.Getter;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 public enum ErrorDesc {
 
-    USERNAME_NOT_FOUND("User not found by given username"),
-    BAD_CREDENTIALS("Bad credentials"),
-    INVALID_TOKEN("Invalid token"),
-    USER_HAS_NOT_SCOPE("Invalid scope requested for user"),
-    BAD_REQUEST("Bad request"),
-    USER_NOT_FOUND("User not found"),
-    AUTHORITY_NOT_FOUND("Authority not found"),
-    ROLE_NOT_FOUND("Role not found"),
-    PARENT_NOT_FOUND("Parent not found"),
-    STUDENT_NOT_FOUND("Student not found"),
-    TEACHING_SUBJECT_NOT_FOUND("Teaching subject not found"),
-    TEACHER_NOT_FOUND("Teacher not found"),
-    GRADE_NOT_FOUND("Grade not found"),
-    APPOINTMENT_NOT_FOUND("Appointment not found"),
-    TEACHER_NOT_AVAILABLE_FOR_APPOINTMENT("The Teacher is not available for the taken appointment"),
-    STUDENT_NOT_AVAILABLE_FOR_APPOINTMENT("The Student is not available for the taken appointment"),
-    SERVER_ERROR("Server error");
+    USERNAME_NOT_FOUND("User not found by given username", 1),
+    BAD_CREDENTIALS("Bad credentials",2),
+    INVALID_TOKEN("Invalid token", 3),
+    USER_HAS_NOT_SCOPE("Invalid scope requested for user", 4),
+    BAD_REQUEST("Bad request", 5),
+    USER_NOT_FOUND("User not found", 6),
+    AUTHORITY_NOT_FOUND("Authority not found", 7),
+    ROLE_NOT_FOUND("Role not found", 8),
+    PARENT_NOT_FOUND("Parent not found", 9),
+    STUDENT_NOT_FOUND("Student not found", 10),
+    TEACHING_SUBJECT_NOT_FOUND("Teaching subject not found", 11),
+    TEACHER_NOT_FOUND("Teacher not found", 12),
+    GRADE_NOT_FOUND("Grade not found", 13),
+    APPOINTMENT_NOT_FOUND("Appointment not found", 14),
+    TEACHER_NOT_AVAILABLE_FOR_APPOINTMENT("The Teacher is not available for the taken appointment", 15),
+    STUDENT_NOT_AVAILABLE_FOR_APPOINTMENT("The Student is not available for the taken appointment", 16),
+    SERVER_ERROR("Server error", 17);
 
     private final String desc;
+    private final Integer code;
+    private static final Map<String, Integer> errorMap =  new HashMap<>();;
 
-    ErrorDesc(String desc) {
+    ErrorDesc(String desc, Integer code) {
         this.desc = desc;
+        this.code = code;
+    }
+
+    static {
+        for (ErrorDesc errorDesc : ErrorDesc.values()) {
+            errorMap.put(errorDesc.getDesc(), errorDesc.getCode());
+        }
+    }
+
+    static public Integer getCodeFormDesc(String desc) {
+        return errorMap.get(desc);
     }
 }
